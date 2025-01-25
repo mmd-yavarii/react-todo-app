4d63b25f15d4f0adee55e5ebb8f23988dea7b1b0
mmd-yavarii <mdyavarii@gmail.com>
2025-01-25 20:05:08 +0330

    clean up header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,39 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 
+const getCurrentGreeting = () => {
+    const hours = new Date().getHours();
+    return hours <= 12
+        ? 'Good Morning'
+        : hours <= 19
+        ? 'Good Evening'
+        : 'Good Night';
+};
+
 const Header = () => {
-    const time = () => {
-        const now = new Date().getHours();
-        let result = '';
-        if (now <= 12) {
-            result = 'Good Morning';
-        } else if (now >= 12 && now <= 19) {
-            result = 'Good Evening';
-        } else {
-            result = 'Good Night';
-        }
-        return result;
-    };
+    const [message] = useState(getCurrentGreeting);
 
     return (
-        <div className={styles.container}>
-            <div>
-                <h2>{time()}</h2>
-                <button>
+        <header className={styles.container}>
+            <div className={styles.headerContent}>
+                <h2>{message}</h2>
+
+                <button aria-label="Filter">
                     <img
                         width="15"
                         height="15"
                         src="https://img.icons8.com/ios/50/filter--v1.png"
-                        alt="filter--v1"
+                        alt="Filter Icon"
                     />
                 </button>
             </div>
 
-            <input type="text" placeholder="Search..." />
-        </div>
+            <input
+                type="text"
+                className={styles.searchInput}
+                placeholder="Search..."
+            />
+        </header>
     );
 };
 
